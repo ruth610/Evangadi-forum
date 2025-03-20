6f6486567e7ba3dc64e3fad01e16b05111a379d6
amirteth <amiraliyimer@gmail.com>
2025-03-19 21:48:50 -0700

    p

--- backend/routes/answerRoute.js
+++ backend/routes/answerRoute.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authMiddleWare = require("../middleware/authMiddleWare");
 
-const { postAnswer, getAnswers } = require("../controller/answerController");
+const {
+  postAnswer,
+  getAnswers,
+  getAnswer
+} = require("../controller/answerController");
 
 // ********** Answer Routes ***************
 
 // Post an answer for a question
 router.post("/", authMiddleWare, postAnswer);
+router.get("/:question_id", getAnswer);
+
