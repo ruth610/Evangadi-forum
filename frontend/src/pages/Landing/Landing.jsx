ac574283b59f0f95bad19847c398e6f7a504e88a
AbiWebDevloper <abebeone2@gmail.com>
2025-03-21 14:23:45 -0600

    feat:about page complete

--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -4,6 +4,7 @@ import Register from "../../components/Auth/Register";
 import styles from "./landing.module.css";
 import homeBg from "./../../assets/home-backgraund.png";
 import Layout from "../../components/Layout/Layout";
+import About from "../../components/About/About";
 // import { useSpring, animated } from 'react-spring';
 function Landing() {
   const [isLogin, setIsLogin] = useState(true);
@@ -23,7 +24,7 @@ function Landing() {
           )}
         </div>
         <div className={styles.landing_about_container}>
-          {/* <About /> */}
+          <About />
         </div>
       </section>
     </Layout>
