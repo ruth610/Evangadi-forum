656059a51e3ef279ecd92770f7fc7f5d9fe152e8
ruth610 <106428054+ruth610@users.noreply.github.com>
2025-09-26 02:07:18 +0300

    update final one

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Instance = axios.create({
-  baseURL: "https://evangadi-forum-backend-i25l.onrender.com/api",
+  baseURL: "https://evangadi-forum-1-4p4r.onrender.com/api",
   // baseURL: "http://localhost:5500/api",
 });
 
